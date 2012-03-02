77893bd5df5bfec4b2438268e9383a0991673b18
Brandon Marken <brandon.marken@gmail.com>
2012-03-01 17:06:36 -0900

    added ip Jumper

---
+++ IPJumper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+'''
+This is a program to randomly change the static IP on a machine ever so often
+Brandon Marken
+This software is licensed under the MIT license
+
+'''
+
+import time
+import random
+import os
+
+def changeIP():
+	comm = "ifconfig eth0 192.168.122."+str(random.randint(0,256))+" netmask 255.255.255.0 up"
+	os.system(comm)
+
+if __name__=='__main__':
+	main()
